d2f80c5c8b291940db95a0bb48313d6ba406cdea
Levani Bukhnikashvili <levanifenix.1234@gmail.com>
2022-06-14 12:49:34 +0400

    Remove target

--- target/classes/com/levaniphoenix/remoteturtle/Lua functions/inspect.lua
+++
@@ -1,7 +0,0 @@
-local table = {}
-table["function"] = "turtle.inspect()"
-table["target"] = "client"
-local result,blockTable = turtle.inspect()
-table["result"] =tostring(result)
-table["block"] = blockTable
-return table
--- target/classes/com/levaniphoenix/remoteturtle/Lua functions/inspectDown.lua
+++
@@ -1,7 +0,0 @@
-local table = {}
-table["function"] = "turtle.inspectDown()"
-table["target"] = "client"
-local result,downTable = turtle.inspectDown()
-table["result"] =tostring(result)
-table["block"] = downTable
-return table
